6429483feda23fcb59995b89fdfbb1fb6223b8c9
GustavoHFMO <guga.h.f.m.o@gmail.com>
2017-08-24 10:44:29 -0300

    upando atualizacoes dos modelos online

--- experimentos/Constantes.py
+++ experimentos/Constantes.py
@@ -13,7 +13,7 @@ class Constantes():
         self.variacao = 4
         self.pasta = "Preliminares"
         if(alocacao == 'dentro'):
-            self.caminho_bases = "../Tabelas/Experimentos/"
+            self.caminho_bases = "../tabelas/Experimentos/"
         elif(alocacao == 'fora'):
-            self.caminho_bases = "Tabelas/Experimentos/"
+            self.caminho_bases = "tabelas/Experimentos/"
         self.folhas = ["ELM", "ELM-DDM", "ELM-ECDD", "ELM-FEDD", "IDPSO-ELM-B", "IDPSO-ELM-S", "P-IDPSO-ELM", "M-IDPSO-ELM"] 
